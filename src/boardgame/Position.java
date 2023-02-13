2e2f2a0a13dd5a8ebd4bd8677979c7830b67f080
Samuel Arruda <Arrudarosasamuel@gmai.com>
2023-02-13 14:43:33 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+public class Position {
+	private int row;
+	private int colunm;
+	
+	public Position(int row, int colunm) {
+		this.row = row;
+		this.colunm = colunm;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColunm() {
+		return colunm;
+	}
+
+	public void setColunm(int colunm) {
+		this.colunm = colunm;
+	}
+	
+	public String toString() {
+		return row + ", " + colunm;
+	}
+}
