c403c79617621db6092eb2d390d0e8cea9309410
Samuel Arruda <Arrudarosasamuel@gmai.com>
2023-02-23 12:05:26 -0300

    Bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,68 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece{
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+	@Override
+	public String toString() {
+		return "B";
+	}
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		p.setValues(position.getRow() - 1, position.getColunm() -1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColunm()] = true;
+			p.setValues(p.getRow() - 1, p.getColunm() - 1);;
+		}
+		if(getBoard().positionExists(p) && isThereOppentPiece(p)) {
+			mat[p.getRow()][p.getColunm()] = true;
+		}
+		
+		p.setValues(position.getRow() - 1, position.getColunm() + 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColunm()]= true;
+			p.setValues(p.getRow() - 1, p.getColunm() + 1);;
+		}
+		if(getBoard().positionExists(p) && isThereOppentPiece(p)) {
+			mat[p.getRow()][p.getColunm()]= true;
+		}
+		
+		
+		p.setValues(position.getRow() + 1, position.getColunm() + 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColunm()]= true;
+			p.setValues(p.getRow() + 1, p.getColunm() + 1);;
+		}
+		if(getBoard().positionExists(p) && isThereOppentPiece(p)) {
+			mat[p.getRow()][p.getColunm()]= true;
+		}
+		
+		
+		p.setValues(position.getRow() + 1, position.getColunm() - 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColunm()] = true;
+			p.setValues(p.getRow() + 1, p.getColunm() - 1);;
+		}
+		if(getBoard().positionExists(p) && isThereOppentPiece(p)) {
+			mat[p.getRow()][p.getColunm()] = true;
+		}
+		return mat;
+	
+		
+		
+	}
+	
+	
+}
+
